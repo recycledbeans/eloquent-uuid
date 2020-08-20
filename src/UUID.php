c1d51656812317274b0ca48a1536b0ff6207889f
Matt Watson <matt@devpartners.io>
2020-08-19 20:18:18 -0400

    Code Standards Fix

--- src/UUID.php
+++ src/UUID.php
@@ -7,7 +7,7 @@ trait UUID
     public static function bootUUID()
     {
         static::creating(function ($model) {
-            $primary_key = (new static)->getKeyName();
+            $primary_key = (new static())->getKeyName();
 
             if (empty($model->$primary_key)) {
                 $model->$primary_key = \Ramsey\Uuid\Uuid::uuid4()->toString();
